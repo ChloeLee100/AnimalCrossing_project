46bd029cdb922652e341af66331b996f9fd0681b
Yeseo Lee <yeseolee@Yeseos-MacBook-Air.local>
2024-12-27 15:49:34 +0900

    game-intro page first commit

--- docs/js/intro.js
+++ docs/js/intro.js
@@ -31,3 +31,39 @@ function sec03Right_Swiper() {
 };
 
 sec03Right_Swiper();
+
+function sec04Left_Swiper() {
+	const swiper = new Swiper('.sec04-left.swiper', {     
+		loop: true,
+		speed: 1000,		
+		autoplay: {
+			delay: 2200,
+		}
+  });
+};
+
+sec04Left_Swiper();
+
+function sec04Mid_Swiper() {
+	const swiper = new Swiper('.sec04-mid.swiper', {     
+		loop: true,
+		speed: 1000,		
+		autoplay: {
+			delay: 2400,
+		}
+  });
+};
+
+sec04Mid_Swiper();
+
+function sec04Right_Swiper() {
+	const swiper = new Swiper('.sec04-right.swiper', {     
+		loop: true,
+		speed: 1000,		
+		autoplay: {
+			delay: 2100,
+		}
+  });
+};
+
+sec04Right_Swiper();
