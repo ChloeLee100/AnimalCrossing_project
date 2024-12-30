ce4639622527da71dd9c976b7732ffad452787ed
Yeseo Lee <yeseolee@Yeseos-MacBook-Air.local>
2024-12-30 20:09:48 +0900

    event page content complete

--- docs/js/event.js
+++ docs/js/event.js
@@ -11,3 +11,49 @@ function mainImg_Swiper() {
 };
 
 mainImg_Swiper();
+
+function showPaging() {
+  const paging01 = document.querySelector(".paging-01");
+  const paging02 = document.querySelector(".paging-02");
+
+  paging02.style.display = "none";
+
+  $(".swiper-next").click(function () {
+    paging01.style.display = "none";
+    paging02.style.display = "block";
+  });
+
+  $(".swiper-prev").click(function () {
+    paging01.style.display = "block";
+    paging02.style.display = "none";
+  });
+};
+
+showPaging();
+
+function showTab() {
+  const tabs = document.querySelectorAll(".tab");
+  const buttons = document.querySelectorAll(".paging ul li");
+
+  tabs.forEach((tab, index) => {
+    if (index === 0) {
+      tab.style.display = "grid"; 
+    } else {
+      tab.style.display = "none";
+    }
+  });
+
+  buttons.forEach((button, index) => {
+    button.addEventListener("click", function () {
+      tabs.forEach((tab) => {
+        tab.style.display = "none";
+      });
+
+      if (tabs[index]) {
+        tabs[index].style.display = "grid";
+      }
+    });
+  });
+};
+
+showTab();
